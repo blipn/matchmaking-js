502c29107c44bea67b239f05d74c3a6342c4d33d
Quentin <quentin.miltgen@atos.net>
2019-04-09 12:09:42 +0200

    better logs

--- src/interface/logs.js
+++ src/interface/logs.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 
 export default (data) => {
-  fs.appendFile('matchmaking.log', `\n${new Date()} : ${JSON.stringify(data)}`, (err) => {
-    if (err) throw err;
-  });
+  if (data.error) {
+    fs.appendFile('matchmaking.log', `\n${new Date()} : ERROR : ${data.error}`, (err) => {
+      if (err) throw err;
+    });
+  } else {
+    fs.appendFile('matchmaking.log', `\n${new Date()} : INFO : ${data.info}`, (err) => {
+      if (err) throw err;
+    });
+  }
 };
