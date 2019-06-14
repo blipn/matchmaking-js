afcf8e11250396aba4865ff873a814a2621601d5
Quentin <quentin.miltgen@atos.net>
2019-06-14 17:26:22 +0200

    new matchmaking logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import Error from './interface/error';
+/* eslint-disable no-plusplus */
+// import Error from './interface/error';
 import Success from './interface/success';
 import { errorLogs, infoLogs } from './interface/logs';
 
-const uuid = require('uuid/v4');
-
 let roomSize = 4;
-let defaultQueue = '_';
-const matchmakingQueues = [];
 const rooms = [];
+let maxRooms = 100;
 
 Success('MATCHMAKING-JS IMPORTED');
 
@@ -16,125 +14,96 @@ export default {
   * Set the module configuration
   * @param {object} config
   * @param {number} config.roomSize
-  * @param {string} config.defaultQueue
+  * @param {number} config.maxRooms
   * @param {boolean} config.errorLogs
   * @param {boolean} config.infoLogs
   * @returns {Success}
   */
   setConfig(config) {
     roomSize = config.roomSize || roomSize;
-    defaultQueue = config.defaultQueue || defaultQueue;
+    maxRooms = config.maxRooms || maxRooms;
     if (typeof config.errorLogs !== 'undefined') errorLogs(config.errorLogs);
     if (typeof config.infoLogs !== 'undefined') infoLogs(config.infoLogs);
+    for (let i = 0; i < maxRooms; i++) {
+      rooms[i] = {
+        name: i,
+        inGame: false,
+        players: [],
+      };
+    }
     return new Success(`Config set to : ${JSON.stringify(config)}`);
   },
 
-  /**
-   * Add a user to the matchmaking queue
-   * @param {Object} user
-   * @param {string} user.id (playername)
-   * @param {number} user.elo (score)
-   * @param {string} queue (queue name)
-   * @returns {Success|Error}
-   */
-  addPlayer(user, queue = defaultQueue) {
-    // Set Player object
-    const player = { queue };
-    if (!user) {
-      return new Error('Missing user object');
+  matchmaker(user) {
+    let found = false;
+    for (let i = 0; i < maxRooms; i++) {
+      // Va en priorité dans une room non vide et non en jeu
+      if (!rooms[i].inGame && rooms[i].players.length > 0 && rooms[i].players.length < roomSize) {
+        found = true;
+        return this.goToRoom(i, user);
+      }
+    } if (!found) {
+      for (let i = 0; i < maxRooms; i++) {
+        // Sinon va dans une room non en jeu
+        if (!rooms[i].inGame && rooms[i].players.length < roomSize) {
+          found = true;
+          return this.goToRoom(i, user);
+        }
+      }
+    } if (!found) {
+      return false; // NO room available
     }
-    if (typeof user === 'string' && user !== '') {
-      player.id = user;
-    } else if (!user.id || typeof user.id !== 'string' || user.id === '') {
-      return new Error('Missing id');
-    } else {
-      player.id = user.id;
+    return false;
+  },
+
+  matchmaking(callback, user) {
+    callback(this.matchmaker(user));
+  },
+
+  goToRoom(roomNumber, user) {
+    if (rooms[roomNumber].players.length >= roomSize || rooms[roomNumber].inGame) {
+      return false; // Cannot join room
     }
-    if (user.elo && typeof user.elo === 'number') {
-      player.elo = user.elo;
-    }
-
-    // Abort if player already in matchmaking
-    if (this.getPlayer(player.id)) {
-      return new Error('Player already in queue');
-    }
-
-    // import player in matchmaking queue
-    matchmakingQueues.push(player);
-
-    // Check if a room is available
-    this.check();
-
-    // notify the player
-    return new Success(`Player ${player.id} added to queue`);
+    rooms[roomNumber].players.push(user);
+    return roomNumber;
   },
 
   /**
-  * See the requested matchmaking queue
-  * or all matchmaking queues by requesting null
-  * @param {string} queue
-  * @returns {object} matchmaking queue
+  * See the requested player room
+  * or all rooms requesting null
+  * @param {string} playername
+  * @returns {Array} rooms
   */
-  status(queue = defaultQueue) {
-    if (queue === null) {
-      return matchmakingQueues;
+  getRooms(playername) {
+    if (playername === null) {
+      return rooms;
     }
-    return matchmakingQueues.filter(i => i.queue === queue);
+    return rooms.filter(i => i.id === playername);
   },
 
   /**
-  * See the current matchmaking queue
+  * Find a player room
   * @param {string} id (playername)
   * @returns {object} player
   */
-  getPlayer(id) {
-    return matchmakingQueues.find(i => i.id === id) || false;
+  findPlayer(id) {
+    return rooms.find(i => i.players.find(p => p === id) === id) || false;
   },
 
   /**
-  * kick a player from matchmaking queue
+  * Kick a player
   * @param {string} id (playername)
-  * @returns {Success}
+  * @returns {boolean} success
   */
   kickPlayer(id) {
-    const player = this.getPlayer(id);
-    const index = matchmakingQueues.indexOf(player);
-
-    if (index !== -1) {
-      matchmakingQueues.splice(index, 1);
+    let room = null;
+    room = this.findPlayer(id).name;
+    if (room !== null) {
+      const index = rooms[room].players.indexOf(id);
+      rooms[room].players.splice(index, 1);
+      return true;
     }
-
-    return new Success(`Player ${JSON.stringify(player)} removed from matchmaking queue`);
+    return false;
   },
 
-  /**
-  * check if a new matchmaking is allowed,
-  * Create rooms and shift players into
-  * @param {string} queue
-  * @returns {Success|Error}
-  */
-  check(queue = defaultQueue) {
-    if (this.status(queue).length >= roomSize) {
-      const players = [];
-      for (let index = 0; index < roomSize; index += 1) {
-        players.push(this.status(queue).shift());
-      }
-      return this.createRoom();
-    }
-    return new Error('Not enough players');
-  },
-
-  /**
-  * Create room
-  * @returns {Success} Success and room object in 'data'
-  */
-  createRoom() {
-    const id = uuid();
-    const room = {
-      id,
-      players: []
-    };
-    rooms.push(room);
-    return new Success(`Room created : ${JSON.stringify(room)}`, room);
-  }
 };
--- test/index.js
+++ test/index.js
@@ -2,46 +2,15 @@ import logs from '../src/interface/logs';
 import { expect } from 'chai';
 import mmjs from '../src';
 
-describe('Testing addPlayer', () => {
-  it('wrong addPlayer properties', () => {
-    expect(mmjs.addPlayer(1)).to.have.property('error');
-  });
-  it('empty addPlayer properties', () => {
-    expect(mmjs.addPlayer()).to.have.property('error');
-  });
-  it('correct addPlayer string', () => {
-    expect(mmjs.addPlayer('test1', 'myQueue')).to.not.have.property('error');
-  });
-  it('player already exists', () => {
-    expect(mmjs.addPlayer('test1')).to.have.property('error');
-  });
-  it('incorrect addPlayer id empty', () => {
-    expect(mmjs.addPlayer({ id: '' })).to.have.property('error');
-  });
-  it('correct addPlayer id valid', () => {
-    expect(mmjs.addPlayer({ id: 'test2' })).to.not.have.property('error');
-  });
-  it('correct addPlayer id and elo', () => {
-    expect(mmjs.addPlayer({ id: 'test3', elo: 10 })).to.not.have.property('error');
-  });
-  it('correct addPlayer elo empty', () => {
-    expect(mmjs.addPlayer({ id: 'test4', elo: '' })).to.not.have.property('error');
-  });
-});
-
-describe('Testing getPlayer', () => {
-  it('get player test3', () => {
-    expect(mmjs.getPlayer('test3')).to.deep.equal({ queue: '_', id: 'test3', elo: 10 });
-  });
-  it('get player 404', () => {
-    expect(mmjs.getPlayer('404')).to.equal(false);
-  });
-});
+function log(room){
+  console.log(room);
+}
 
 describe('Testing setConfig', () => {
   it('set valid config', () => {
     expect(mmjs.setConfig({
       roomSize: 6,
+      maxRooms: 3,
       defaultQueue: '*',
       errorLogs: true,
       infoLogs: true
@@ -49,32 +18,83 @@ describe('Testing setConfig', () => {
   });
 });
 
-describe('Testing kickPlayer', () => {
-  it('kick player test3', () => {
-    expect(mmjs.kickPlayer('test3')).to.have.property('info');
-    expect(mmjs.kickPlayer('test4')).to.have.property('info');
-  });
-  it('cannot get player test3', () => {
-    expect(mmjs.getPlayer('test3')).to.equal(false);
-  });
-});
-
-describe('Testing status', () => {
-  it('get default queue', () => {
-    expect(mmjs.status()).to.deep.equal([]);
-  });
-  it('get all matchmaking queues', () => {
-    expect(mmjs.status(null)).to.deep.equal([ { queue: 'myQueue', id: 'test1' }, { queue: '_', id: 'test2' } ]);
-  });
-  it('get \'myQueue\' matchmaking queue', () => {
-    expect(mmjs.status('myQueue')).to.deep.equal([ { queue: 'myQueue', id: 'test1' } ]);
-  });
-});
-
 describe('Testing matchmaking', () => {
   it('many players matchmaking', () => {
     for (let index = 0; index < 10; index++) {
-      mmjs.addPlayer(`player${index}`);
+      mmjs.matchmaking(log, `player${index}`);
+      if(index === 9) {
+        expect(mmjs.getRooms()).to.deep.equal([
+            {
+              "inGame": false,
+              "name": 0,
+              "players": [
+                "player0",
+                "player1",
+                "player2",
+                "player3",
+                "player4",
+                "player5"
+              ]
+            },
+            {
+              "inGame": false,
+              "name": 1,
+              "players": [
+                "player6",
+                "player7",
+                "player8",
+                "player9"
+              ]
+            },
+            {
+              "inGame": false,
+              "name": 2,
+              "players": []
+            }
+        ]);
+      }
     }
+  });
+});
+
+describe('Testing getPlayer', () => {
+  it('find player player7', () => {
+    expect(mmjs.findPlayer('player7')).to.deep.equal({
+      "inGame": false,
+      "name": 1,
+      "players": [
+        "player6",
+        "player7",
+        "player8",
+        "player9"
+      ]
+    });
+  });
+  it('get player 404', () => {
+    expect(mmjs.findPlayer('404')).to.equal(false);
+  });
+});
+
+
+describe('Testing kickPlayer', () => {
+  it('can get player player3', () => {
+    expect(mmjs.findPlayer('player3')).to.deep.equal({
+      "inGame": false,
+      "name": 0,
+      "players": [
+        "player0",
+        "player1",
+        "player2",
+        "player3",
+        "player4",
+        "player5"
+      ]
+    });
+  });
+  it('kick player player3', () => {
+    expect(mmjs.kickPlayer('player3')).to.equal(true);
+  });
+  it('cannot get player player3', () => {
+    expect(mmjs.findPlayer('player3')).to.equal(false);
   });
 });
